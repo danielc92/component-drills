7a20a05008ec7833ccbf9224a26d70bcdb88dbd2
Daniel Corcoran <danielc.92@outlook.com>
2020-03-27 12:06:13 +1100

    Added Basic Banner

---
+++ src/components/banners/Basic/index.js
@@ -0,0 +1,27 @@
+import React from "react"
+import styled from "styled-components"
+
+const Banner = styled.div`
+  background: ${props => props.background};
+  color: white;
+  text-align: center;
+  padding: 48px 0;
+`
+
+const Header = styled.h1`
+  font-size: 56px;
+  margin-bottom: 24px;
+`
+
+const SubHeader = styled.p`
+  font-size: 18px;
+  margin-bottom: 24px;
+`
+export default function index({ header, subHeader, background }) {
+  return (
+    <Banner background={background}>
+      <Header>{header}</Header>
+      <SubHeader>{subHeader}</SubHeader>
+    </Banner>
+  )
+}
