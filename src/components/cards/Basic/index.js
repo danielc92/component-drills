4878fd232d8a5f2770f90850b4659118877f931e
Daniel Corcoran <danielc.92@outlook.com>
2020-03-28 22:41:10 +1100

    Added tags

--- src/components/cards/Basic/index.js
+++ src/components/cards/Basic/index.js
@@ -21,9 +21,25 @@ const Header = styled.h3`
 `
 
 const Paragraph = styled.p`
-  margin: 0;
+  margin: 0 0 12px 0;
 `
 
+const TagGroup = styled.div`
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+`
+
+const Tag = styled.div`
+  display: inline-block;
+  background: ${props => (props.background ? props.background : "darkgray")};
+  color: #fff;
+  font-size: 10px;
+  padding: 4px;
+  margin: 0 4px 4px 0;
+
+  border-radius: 2px;
+`
 function index() {
   return (
     <Card>
@@ -37,6 +53,12 @@ function index() {
           consequat amet. Minim esse id aliqua minim sit cupidatat fugiat
           consequat incididunt nulla.
         </Paragraph>
+        <TagGroup>
+          <Tag background="red"># scene</Tag>
+          <Tag background="blue"># photo</Tag>
+          <Tag background="purple"># imagery</Tag>
+          <Tag background="blue"># something</Tag>
+        </TagGroup>
       </CardContent>
     </Card>
   )
