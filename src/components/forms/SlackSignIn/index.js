372f684dd55f8e5c630712944404c9fbbf136c98
Daniel Corcoran <danielc.92@outlook.com>
2020-04-03 17:59:00 +1100

    Added slack sign in form

---
+++ src/components/forms/SlackSignIn/index.js
@@ -0,0 +1,93 @@
+import React from "react"
+import styled from "styled-components"
+
+const Wrapper = styled.section`
+  max-width: 700px;
+  margin: 0 auto;
+  border-radius: 2px;
+  border: 1px solid #dcdcdc;
+  text-align: center;
+`
+
+const Content = styled.div`
+  padding: 40px 125px;
+  display: grid;
+  gap: 24px;
+`
+const Title = styled.h1`
+  font-size: 36px;
+  margin: 0;
+`
+
+const SubTitle = styled.p`
+  margin: 0;
+`
+
+const InputWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const Input = styled.input`
+  padding: 20px;
+  width: 200px;
+  margin-right: 8px;
+  text-align: right;
+  font-size: 18px;
+  outline: none;
+  border: 1px solid grey;
+  border-radius: 4px;
+  ::placeholder {
+    color: grey;
+  }
+`
+
+const InputHelpText = styled.p`
+  margin: 0;
+  font-size: 18px;
+  font-weight: bold;
+`
+
+const Button = styled.a`
+  display: block;
+  background: #007a5a;
+  color: white;
+  font-weight: bold;
+  padding: 16px;
+  border-radius: 4px;
+  font-size: 18px;
+`
+
+const Link = styled.a`
+  color: #1264a3;
+  text-decoration: none;
+  font-weight: bold;
+`
+
+export default function index({
+  title,
+  subtitle,
+  placeholder,
+  buttonText,
+  inputSideText,
+  helpText,
+  link
+}) {
+  return (
+    <Wrapper>
+      <Content>
+        <Title>{title}</Title>
+        <SubTitle>{subtitle}</SubTitle>
+        <InputWrapper>
+          <Input placeholder={placeholder} />
+          <InputHelpText>{` ${inputSideText}`}</InputHelpText>
+        </InputWrapper>
+        <Button>{buttonText}</Button>
+        <SubTitle>
+          {helpText}
+          <Link href={link.url}>{link.text}</Link>
+        </SubTitle>
+      </Content>
+    </Wrapper>
+  )
+}
--- src/stories/forms.stories.js
+++ src/stories/forms.stories.js
@@ -1,9 +1,23 @@
 import NewYorker from "../components/forms/NewYorker"
+import Slack from "../components/forms/SlackSignIn"
+
 import React from "react"
 
 export default {
   title: "Forms"
 }
+
+export const SlackSignInForm = () => (
+  <Slack
+    title="Sign in to your workspace"
+    subtitle="Enter your workspace's flack URL"
+    placeholder="your-workspace-url"
+    buttonText="Continue"
+    helpText="Don't know your workspace URL? "
+    link={{ text: "Find your workspace", url: "#" }}
+    inputSideText=".flack.com"
+  />
+)
 
 export const NewYorkerForm = () => (
   <NewYorker
