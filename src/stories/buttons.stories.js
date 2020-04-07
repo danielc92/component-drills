eddf08afc0fc0ab5c189d5992b78615dc06c589e
Daniel Corcoran <danielc.92@outlook.com>
2020-04-07 21:32:14 +1000

    Added a button wrapper

--- src/stories/buttons.stories.js
+++ src/stories/buttons.stories.js
@@ -1,22 +1,49 @@
 import React from "react"
 import "../App.css"
+import Dev from "../components/buttons/DevButton"
 import OutlookButton from "../components/buttons/OutlookButton"
 import StorybookButton from "../components/buttons/StorybookButton"
+import styled from "styled-components"
+
 export default {
   title: "Buttons",
-  component: OutlookButton
+  component: OutlookButton,
 }
 
+// A wrapper to center buttons
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
 export const OutlookButtonRegular = () => (
-  <OutlookButton>Create an account</OutlookButton>
+  <ButtonWrapper>
+    <OutlookButton>Create an account</OutlookButton>
+  </ButtonWrapper>
 )
 
 export const OutlookButtonHollow = () => (
-  <OutlookButton hollow>Create an account</OutlookButton>
+  <ButtonWrapper>
+    <OutlookButton hollow>Create an account</OutlookButton>
+  </ButtonWrapper>
 )
 
-export const StorybookPrimary = () => <StorybookButton text="Click me" />
+export const DevButton = () => (
+  <ButtonWrapper>
+    <Dev buttonText="Click me" buttonHref="#" />
+  </ButtonWrapper>
+)
+
+export const StorybookPrimary = () => (
+  <ButtonWrapper>
+    <StorybookButton text="Click me" />
+  </ButtonWrapper>
+)
 
 export const StorybookSecondary = () => (
-  <StorybookButton secondary text="Click me" />
+  <ButtonWrapper>
+    <StorybookButton secondary text="Click me" />
+  </ButtonWrapper>
 )
