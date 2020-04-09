420c6f41935cda29655de5a551408bbb6df6b406
Daniel Corcoran <danielc.92@outlook.com>
2020-04-09 17:14:50 +1000

    Added Reddit communities

--- src/stories/lists.stories.js
+++ src/stories/lists.stories.js
@@ -2,14 +2,22 @@ import React from "react"
 import "../App.css"
 import faker from "faker"
 import VideoList from "../components/lists/VideoList"
+import RedditTrending from "../components/lists/RedditTrendingList"
 import FacebookFriends from "../components/lists/FacebookFriendList"
 import face1 from "./images/face-1.png"
 import face2 from "./images/face-2.png"
 import face3 from "./images/face-3.png"
 import face4 from "./images/face-4.png"
 import face5 from "./images/face-5.png"
+import icon1 from "./images/compass.png"
+import icon2 from "./images/diamond.png"
+import icon3 from "./images/database-2.png"
+import icon4 from "./images/compose.png"
+import icon5 from "./images/internet.png"
+
+import { CenterHorizontalAndVertical } from "./layout"
 export default {
-  title: "Lists"
+  title: "Lists",
 }
 
 const data = [
@@ -17,30 +25,30 @@ const data = [
     title: "Corona Virus Outbreak",
     studio: "Channel 8",
     imageSrc: "https://picsum.photos/id/237/600/450",
-    time: "12:32"
+    time: "12:32",
   },
   {
     title: "Corona Virus Outbreak",
     studio: "Channel 8",
     imageSrc: "https://picsum.photos/id/233/600/450",
-    time: "12:32"
+    time: "12:32",
   },
   {
     title: "Corona Virus Outbreak",
     studio: "Channel 8",
     imageSrc: "https://picsum.photos/id/232/600/450",
-    time: "12:32"
+    time: "12:32",
   },
   {
     title: "Corona Virus Outbreak",
     studio: "Channel 8",
     imageSrc: "https://picsum.photos/id/231/600/450",
-    time: "12:32"
-  }
+    time: "12:32",
+  },
 ]
 
 export const YoutubeVideoList = () => {
-  return data.map(item => <VideoList {...item} />)
+  return data.map((item) => <VideoList {...item} />)
 }
 
 const friends = [
@@ -49,35 +57,70 @@ const friends = [
     name: faker.name.firstName() + " " + faker.name.lastName(),
     buttonText: "Add friend",
     buttonText2: "Remove",
-    mutualText: "5 Mutual friends."
+    mutualText: "5 Mutual friends.",
   },
   {
     src: face2,
     name: faker.name.firstName() + " " + faker.name.lastName(),
     buttonText: "Add friend",
     buttonText2: "Remove",
-    mutualText: "5 Mutual friends."
+    mutualText: "5 Mutual friends.",
   },
   {
     src: face3,
     name: faker.name.firstName() + " " + faker.name.lastName(),
     buttonText: "Add friend",
     buttonText2: "Remove",
-    mutualText: "5 Mutual friends."
+    mutualText: "5 Mutual friends.",
   },
   {
     src: face4,
     name: faker.name.firstName() + " " + faker.name.lastName(),
     buttonText: "Add friend",
     buttonText2: "Remove",
-    mutualText: "5 Mutual friends."
+    mutualText: "5 Mutual friends.",
   },
   {
     src: face5,
     name: faker.name.firstName() + " " + faker.name.lastName(),
     buttonText: "Add friend",
     buttonText2: "Remove",
-    mutualText: "5 Mutual friends."
-  }
+    mutualText: "5 Mutual friends.",
+  },
 ]
 export const FacebookFriendList = () => <FacebookFriends friends={friends} />
+export const RedditTrendingCommunities = () => (
+  <CenterHorizontalAndVertical>
+    {" "}
+    <RedditTrending
+      items={[
+        {
+          imageSrc: icon1,
+          members: "7.2k",
+          name: "r/Consequat adipisic",
+        },
+        {
+          imageSrc: icon2,
+          members: "7.2k",
+          name: "r/Exercitation",
+        },
+        {
+          imageSrc: icon3,
+          members: "1.2m",
+          name: "r/Exercitation",
+        },
+        {
+          imageSrc: icon4,
+          members: "700",
+          name: "r/Exercitation",
+        },
+        {
+          imageSrc: icon5,
+          members: "8.2k",
+          name: "r/Exercitation",
+        },
+      ]}
+      title="Trending Communities"
+    />
+  </CenterHorizontalAndVertical>
+)
