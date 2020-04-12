1803e5901c2a93d6d3c2b47d40a09eb19cb020e1
Daniel Corcoran <danielc.92@outlook.com>
2020-04-12 16:42:50 +1000

    Added some grids

---
+++ src/components/tiles/Grid3x3/index.js
@@ -0,0 +1,45 @@
+import React from "react"
+import styled from "styled-components"
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+
+  gap: ${(props) => (props.gapless ? "" : "24px")};
+  max-width: 800px;
+`
+
+const GridTile = styled.div`
+  background-color: ${(props) => props.backgroundColor};
+  color: ${(props) => props.color};
+  padding: 24px;
+  line-height: 1.5;
+  background-size: cover;
+  background-position: 50% 50%;
+  background-repeat: no-repeat;
+  background-image: url(${(props) => (props.imageSrc ? props.imageSrc : "")});
+  grid-row-start: ${(props) => (props.doubleSize ? "span 2" : "")};
+  grid-column-start: ${(props) => (props.doubleSize ? "span 2" : "")};
+`
+
+const GridTitle = styled.h2`
+  margin: 0 0 12px 0;
+`
+
+export default function index({ items, gapless }) {
+  return (
+    <Grid gapless={gapless}>
+      {items.map((i) => (
+        <GridTile
+          doubleSize={i.doubleSize}
+          color={i.color}
+          backgroundColor={i.backgroundColor}
+          imageSrc={i.imageSrc}
+        >
+          <GridTitle>{i.title}</GridTitle>
+          {i.text}
+        </GridTile>
+      ))}
+    </Grid>
+  )
+}
