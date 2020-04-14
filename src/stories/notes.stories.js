561a9ae517962b049842ee25e71e922e5ec63cb0
Daniel Corcoran <danielc.92@outlook.com>
2020-04-14 20:17:03 +1000

    Added notes to stories

---
+++ src/stories/notes.stories.js
@@ -0,0 +1,68 @@
+import React from "react"
+import { CenterHorizontalAndVertical } from "./layout"
+import Basic from "../components/notes/Basic"
+export default {
+  title: "Notes",
+}
+
+export const BasicDefault = () => (
+  <CenterHorizontalAndVertical>
+    <Basic
+      title="Officia aliquip eu ad"
+      text="Dolore por fugiat cupidatat aliqua dolore incididunt. Cillum incididunt cupidatat dolor dolor Lorem duis reprehenderit ea magna duis officia. Eiusmod incididunt ipsum dolore voluptate ipsum ullamco. Tempor exercitation reprehenderit quis officia aliqua fugiat mollit adipisicing mollit exercitation ut ea laboris. Sint enim id ea eiusmod. Aute aliquip consequat labore consectetur dolore elit ut laborum."
+      type=""
+    />
+  </CenterHorizontalAndVertical>
+)
+
+export const BasicDanger = () => (
+  <CenterHorizontalAndVertical>
+    <Basic
+      title="Officia aliquip eu ad"
+      text="Dolore por fugiat cupidatat aliqua dolore incididunt. Cillum incididunt cupidatat dolor dolor Lorem duis reprehenderit ea magna duis officia. Eiusmod incididunt ipsum dolore voluptate ipsum ullamco. Tempor exercitation reprehenderit quis officia aliqua fugiat mollit adipisicing mollit exercitation ut ea laboris. Sint enim id ea eiusmod. Aute aliquip consequat labore consectetur dolore elit ut laborum."
+      type="danger"
+    />
+  </CenterHorizontalAndVertical>
+)
+
+export const BasicSuccess = () => (
+  <CenterHorizontalAndVertical>
+    <Basic
+      title="Officia aliquip eu ad"
+      text="Dolore por fugiat cupidatat aliqua dolore incididunt. Cillum incididunt cupidatat dolor dolor Lorem duis reprehenderit ea magna duis officia. Eiusmod incididunt ipsum dolore voluptate ipsum ullamco. Tempor exercitation reprehenderit quis officia aliqua fugiat mollit adipisicing mollit exercitation ut ea laboris. Sint enim id ea eiusmod. Aute aliquip consequat labore consectetur dolore elit ut laborum."
+      type="success"
+    />
+  </CenterHorizontalAndVertical>
+)
+
+export const BasicPrimary = () => (
+  <CenterHorizontalAndVertical>
+    <Basic
+      title="Officia aliquip eu ad"
+      text="Dolore por fugiat cupidatat aliqua dolore incididunt. Cillum incididunt cupidatat dolor dolor Lorem duis reprehenderit ea magna duis officia. Eiusmod incididunt ipsum dolore voluptate ipsum ullamco. Tempor exercitation reprehenderit quis officia aliqua fugiat mollit adipisicing mollit exercitation ut ea laboris. Sint enim id ea eiusmod. Aute aliquip consequat labore consectetur dolore elit ut laborum."
+      type="primary"
+    />
+  </CenterHorizontalAndVertical>
+)
+
+export const BasicSuccessRounded = () => (
+  <CenterHorizontalAndVertical>
+    <Basic
+      rounded
+      title="Officia aliquip eu ad"
+      text="Dolore por fugiat cupidatat aliqua dolore incididunt. Cillum incididunt cupidatat dolor dolor Lorem duis reprehenderit ea magna duis officia. Eiusmod incididunt ipsum dolore voluptate ipsum ullamco. Tempor exercitation reprehenderit quis officia aliqua fugiat mollit adipisicing mollit exercitation ut ea laboris. Sint enim id ea eiusmod. Aute aliquip consequat labore consectetur dolore elit ut laborum."
+      type="success"
+    />
+  </CenterHorizontalAndVertical>
+)
+export const BasicSuccessRoundedLeft = () => (
+  <CenterHorizontalAndVertical>
+    <Basic
+      rounded
+      title="Officia aliquip eu ad"
+      text="Dolore por fugiat cupidatat aliqua dolore incididunt. Cillum incididunt cupidatat dolor dolor Lorem duis reprehenderit ea magna duis officia. Eiusmod incididunt ipsum dolore voluptate ipsum ullamco. Tempor exercitation reprehenderit quis officia aliqua fugiat mollit adipisicing mollit exercitation ut ea laboris. Sint enim id ea eiusmod. Aute aliquip consequat labore consectetur dolore elit ut laborum."
+      type="success"
+      borderStyle="left"
+    />
+  </CenterHorizontalAndVertical>
+)
