28b53f8a5baa80d5c1f26365cca58915e3a87345
Daniel Corcoran <danielc.92@outlook.com>
2020-08-06 21:08:06 +1000

    Modfiied story

--- src/stories/carousels.stories.tsx
+++ src/stories/carousels.stories.tsx
@@ -3,14 +3,16 @@ import Medium from "../components/carousels/Medium"
 import faker from "faker"
 
 export default {
-  title: "Carousels"
+  title: "Carousels",
 }
 
-const items = new Array(5).fill(1).map(i => ({
+const items: MediumCarouselItem[] = new Array(5).fill(1).map((i) => ({
   text:
     "Ipsum sint ut excepteur fugiat esse. Ut duis ut sint enim sunt quis qui aute msint enim sunt quis qui aute magna labore sint velit id. Qui aliquip exercitation sunt do nostrud.",
   authorName: faker.name.firstName() + " " + faker.name.lastName(),
-  authorUrl: faker.image.imageUrl()
+  authorUrl: faker.image.imageUrl(),
 }))
 
-export const MediumCarousel = () => <Medium items={items} />
+export const MediumCarousel = () => (
+  <Medium items={items} ctaText="Lorem ipsum delor" />
+)
