0f2cd820ad57e7b3f0a3908960bd50694c0f85be
Daniel Corcoran <danielc.92@outlook.com>
2021-02-16 10:46:10 +1100

    added a11y

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   stories: ["../src/**/*.stories.tsx"],
   addons: [
+    "@storybook/addon-a11y",
     "@storybook/addon-knobs/register",
     "@storybook/preset-create-react-app",
   ],
